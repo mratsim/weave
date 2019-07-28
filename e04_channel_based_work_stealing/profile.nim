fd04d2f51e59fd93dc6364c1322aedc55762e790
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-07-28 19:53:47 +0200

    [Channel WS] Implement profiling

---
+++ e04_channel_based_work_stealing/profile.nim
@@ -0,0 +1,81 @@
+import
+  # Internal
+  ./timer
+
+# TODO: use runtime cpu frequency detection
+const
+  CpuFreqMhz {.intdefine.} = 4100
+  CpuFreqGhz = CpuFreqMhz.float64 / 100
+
+template checkName(name: untyped) =
+  static:
+    if astToStr(name) notin ["run_task", "enq_deq_task", "send_recv_task", "send_recv_req", "idle"]:
+      raise newException(
+        ValueError,
+        "Invalid profile name: \"" & astToStr(name) & "\"\n" &
+          """Only "run_task", "enq_deq_task", "send_recv_task", "send_recv_req", "idle" are valid"""
+      )
+
+template profile_decl*(name: untyped): untyped {.dirty.} =
+  checkName(name)
+  var `timer _ name`{.inject.}: Timer
+
+template profile_extern_decl*(name: untyped): untyped {.dirty.} =
+  checkName(name)
+  var `timer _ name`*{.inject.}: Timer
+
+template profile_init*(name: untyped): untyped {.dirty.} =
+  checkName(name)
+  timer_new(`timer _ name`, CpuFreqGhz)
+
+template profile_start*(name: untyped): untyped {.dirty.} =
+  checkName(name)
+  timer_start(`timer _ name`)
+
+template profile_stop*(name: untyped): untyped {.dirty.} =
+  checkName(name)
+  timer_stop(`timer _ name`)
+
+proc printf(formatstr: cstring) {.header: "<stdio.h>", varargs.}
+  # Nim interpolation with "%" doesn't support formatting
+  # And strformat requires inlining the variable with the format
+
+template profile_results*(): untyped {.dirty.} =
+  # Parsable format
+  # The first value should make it easy to grep for these lines, e.g. with
+  # ./a.out | grep Timer | cut -d, -f2-
+  # Worker ID, Task, Send/Recv Req, Send/Recv Task, Enq/Deq Task, Idle, Total
+  printf(
+    "Timer,%d,%.3lf,%.3lf,%.3lf,%.3lf,%.3lf,%.3lf\n",
+    ID, # Captured from environment
+    timer_elapsed(timer_run_task, tkMicroseconds),
+    timer_elapsed(timer_send_recv_req, tkMicroseconds),
+    timer_elapsed(timer_send_recv_task, tkMicroseconds),
+    timer_elapsed(timer_enq_deq_task, tkMicroseconds),
+    timer_elapsed(timer_idle, tkMicroseconds),
+    timers_elapsed(
+      timer_run_task,
+      timer_send_recv_req,
+      timer_send_recv_task,
+      timer_enq_deq_task,
+      timer_idle,
+      tkMicroseconds
+    )
+  )
+
+when isMainModule:
+  let ID = 0
+
+  profile_decl(run_task)
+  profile_decl(send_recv_req)
+  profile_decl(send_recv_task)
+  profile_decl(enq_deq_task)
+  profile_decl(idle)
+
+  profile_init(run_task)
+  profile_init(send_recv_req)
+  profile_init(send_recv_task)
+  profile_init(enq_deq_task)
+  profile_init(idle)
+
+  profile_results()
--- e04_channel_based_work_stealing/timer.nim
+++ e04_channel_based_work_stealing/timer.nim
@@ -3,12 +3,12 @@
 # ----------------------------------------------------------------------------------
 
 type
-  Timer = object
+  Timer* = object
     start, stop: int64
     elapsed: int64
     ghz: float64
 
-  TimerKind = enum
+  TimerKind* = enum
     tkMicroseconds
     tkMilliseconds
     tkSeconds
@@ -17,29 +17,26 @@ func cycles_per_sec(t: float64): float64 {.inline.}  = t * 1e9
 func cycles_per_msec(t: float64): float64 {.inline.} = t * 1e6
 func cycles_per_usec(t: float64): float64 {.inline.} = t * 1e3
 
-when defined(i386 or amd64):
+when defined(i386) or defined(amd64):
   # From Linux
   #
-	# The RDTSC instruction is not ordered relative to memory
-	# access.  The Intel SDM and the AMD APM are both vague on this
-	# point, but empirically an RDTSC instruction can be
-	# speculatively executed before prior loads.  An RDTSC
-	# immediately after an appropriate barrier appears to be
-	# ordered as a normal load, that is, it provides the same
-	# ordering guarantees as reading from a global memory location
-	# that some other imaginary CPU is updating continuously with a
-	# time stamp.
-	#
-	# Thus, use the preferred barrier on the respective CPU, aiming for
-	# RDTSCP as the default.
+  # The RDTSC instruction is not ordered relative to memory
+  # access.  The Intel SDM and the AMD APM are both vague on this
+  # point, but empirically an RDTSC instruction can be
+  # speculatively executed before prior loads.  An RDTSC
+  # immediately after an appropriate barrier appears to be
+  # ordered as a normal load, that is, it provides the same
+  # ordering guarantees as reading from a global memory location
+  # that some other imaginary CPU is updating continuously with a
+  # time stamp.
   #
   # From Intel SDM
   # https://www.intel.com/content/dam/www/public/us/en/documents/white-papers/ia-32-ia-64-benchmark-code-execution-paper.pdf
   when not defined(vcc):
     when defined(amd64):
       proc getticks(): int64 {.inline, sideeffect.} =
-        var lo, hi: uint64
-        # TODO: autodetect RDTSCP support
+        var lo, hi: int64
+        # TODO: Provide a compile-time flag for RDTSCP support
         #       and use it instead of lfence + RDTSC
         {.emit: """asm volatile(
           "lfence"
@@ -51,7 +48,7 @@ when defined(i386 or amd64):
         return (hi shl 32) or lo
     else:
       proc getticks(): int64 {.inline, sideeffect.} =
-        # TODO: autodetect RDTSCP support
+        # TODO: Provide a compile-time flag for RDTSCP support
         #       and use it instead of lfence + RDTSC
         {.emit: """asm volatile(
           "lfence"
@@ -70,25 +67,30 @@ when defined(i386 or amd64):
 else:
   {.error: "getticks is not supported on this CPU architecture".}
 
-func timer_new(timer: var Timer, ghz: float64) {.inline.}=
+func timer_new*(timer: var Timer, ghz: float64) {.inline.}=
   timer.elapsed = 0
   timer.ghz = ghz
 
 func timer_reset(timer: var Timer, ghz: float64) {.inline.}=
   timer_new(timer, ghz)
 
-proc timer_start(timer: var Timer) =
+proc timer_start*(timer: var Timer) {.inline.}=
   timer.start = getticks()
 
-proc timer_end(timer: var Timer) =
+proc timer_end*(timer: var Timer) {.inline.}=
   timer.stop = getticks()
   timer.elapsed += timer.stop - timer.start
 
-func timer_elapsed(timer: Timer, timerKind: TimerKind): float64 =
+func timer_elapsed*(timer: Timer, timerKind: TimerKind): float64 {.inline.}=
   case timerKind
   of tkMicroseconds:
-    result = timer.elapsed / cycles_per_usec(timer.ghz)
+    result = timer.elapsed.float64 / cycles_per_usec(timer.ghz)
   of tkMilliseconds:
-    result = timer.elapsed / cycles_per_msec(timer.ghz)
+    result = timer.elapsed.float64 / cycles_per_msec(timer.ghz)
   of tkSeconds:
-    result = timer.elapsed / cycles_per_sec(timer.ghz)
+    result = timer.elapsed.float64 / cycles_per_sec(timer.ghz)
+
+func timers_elapsed*(timers: varargs[Timer],
+                    timerKind: TimerKind): float64 {.inline.}=
+  for timer in timers:
+    result += timer_elapsed(timer, timerKind)
