15941860a824e342d3b6fc546474fcaa38d45224
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-07-29 10:28:07 +0200

    [Channel WS] Add platform definition

---
+++ e04_channel_based_work_stealing/platform.nim
@@ -0,0 +1,30 @@
+from strutils import parseEnum
+
+type
+  StealKind {.pure.}= enum
+    one
+    half
+    adaptative
+
+  SplitKind {.pure.}= enum
+    half
+    guided
+    adaptative
+
+const
+  Platform{.strdefine.} = "SHM"
+  Steal{.strdefine.} = "one"
+  Split{.strdefine.} = "half"
+
+  StealStrategy = parseEnum[StealKind](Steal)
+  SplitStrategy = parseEnum[SplitKind](Split)
+
+when Platform == "SHM":
+  {.pragma: private, threadvar.}
+elif Platform == "SCC":
+  {.pragma: private.}
+else:
+  static:
+    echo "Invalid platform \"", Platform &
+      "\". Only SHM (Shared Memory) and SCC (Intel Single Chip Cloud experimental CPU) " &
+      "are supported"
