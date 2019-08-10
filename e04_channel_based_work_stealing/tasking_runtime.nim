ff8b866fe5002e16052335110097d2bdca922e3e
Mamy Ratsimbazafy <mratsim@users.noreply.github.com>
2019-08-10 20:21:13 +0200

    OSX compat (#1)
    * OSX compat - implement barriers
    
    * Detect TLS emulation
    
    * OSX: broadcast instad of signal condition variable

--- e04_channel_based_work_stealing/tasking_runtime.nim
+++ e04_channel_based_work_stealing/tasking_runtime.nim
@@ -13,8 +13,12 @@ import
 # pthread_create initializer
 # ----------------------------------------------------------------------------------
 
+when compileOption("tlsemulation"):
+  {.error: "This requires --tlsemulation:off compilation flag (default on Linux, not default on OSX)".}
+
 proc worker_entry_fn(id: ptr int32): pointer {.noconv.} =
-  ID = id[]
+  let a = id[]
+  ID = a # If this crashes, you need --tlsemulation:off
   set_current_task(nil)
   num_tasks_exec = 0
   tasking_finished = false
@@ -48,14 +52,7 @@ proc tasking_internal_init*() =
   else:
     num_workers = countProcessors().int32
 
-  # TODO Question, why the convoluted cpu_count()
-  # when countProcessors / sysconf(_SC_NPROCESSORS_ONLN)
-  # is easy
-  #
-  # Call cpu_count() only once, before changing the affinity of thread 0!
-  # After set_thread_affinity(0), cpu_count() would return 1, and every
-  # thread would end up being pinned to processor 0.
-  let num_cpus {.global.} = cpu_count()
+  let num_cpus {.global.} = countProcessors().int32
   printf("Number of CPUs: %d\n", num_cpus)
 
   when defined(DISABLE_MANAGER):
