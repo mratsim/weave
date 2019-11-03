be764ff0c645f308d49cc80ad75bde7d870decc7
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-11-03 23:59:16 +0100

    Add a channel work stealing example

--- e04_channel_based_work_stealing.nim
+++ e04_channel_based_work_stealing.nim
@@ -3,3 +3,41 @@
 # Characteristics:
 # - Message-passing based:
 #   "Share memory by communicating instead of communicating by sharing."
+
+import
+  # stdlib
+  os, strutils, times, strformat,
+  # runtime
+  ./e04_channel_based_work_stealing/[tasking, runtime, async_internal]
+
+proc parfib(n: uint64): uint64 =
+  if n < 2:
+    return n
+
+  let x = async parfib(n-1)
+  let y = parfib(n-2)
+
+  return await(x) + y
+
+proc main() =
+  if paramCount() != 1:
+    echo "Usage: fib <n-th fibonacci number requested>"
+    quit 0
+
+  let n = paramStr(1).parseUInt.uint64
+
+  tasking_init()
+
+  let start = epochTime()
+  let f = parfib(n)
+  let stop = epochTime()
+
+  echo &"Result: {f}"
+  echo &"Elapsed wall time: {stop-start:2.6f} s"
+
+  tasking_barrier()
+  tasking_exit()
+
+main()
+
+# Compile with
