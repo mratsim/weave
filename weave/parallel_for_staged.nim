f0129a0c620a7c43b9ab35b1c5546ab9bea9afb8
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-12-23 22:02:33 +0100

    Switch the sync() global barrier to a finite state automaton

--- weave/parallel_for_staged.nim
+++ weave/parallel_for_staged.nim
@@ -143,7 +143,7 @@ macro parallelForStagedStrided*(loopParams: untyped, stride: Positive, body: unt
 # --------------------------------------------------------
 
 when isMainModule:
-  import ./runtime
+  import ./runtime, ./runtime_fsm
 
   block:
     # expandMacros:
