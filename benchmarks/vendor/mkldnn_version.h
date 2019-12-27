338d3727994acb711cc4f1232d901c40e3157bf8
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2019-12-27 12:53:27 +0100

    Use an older MKL-DNN (1.0.4) that did not entangle everything with cpu_engine.cpp

---
+++ benchmarks/vendor/mkldnn_version.h
@@ -0,0 +1,32 @@
+/*******************************************************************************
+* Copyright 2019 Intel Corporation
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*******************************************************************************/
+
+#ifndef MKLDNN_VERSION_H
+#define MKLDNN_VERSION_H
+
+/// Major version
+#define MKLDNN_VERSION_MAJOR 1
+
+/// Minor version
+#define MKLDNN_VERSION_MINOR 0
+
+/// Patch version
+#define MKLDNN_VERSION_PATCH 4
+
+/// Git commit hash
+#define MKLDNN_VERSION_HASH  "a0a87d662edeef38d01db4ac5dd25f59a1f0881f"
+
+#endif
