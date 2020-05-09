bd762f92647b38c3bd55e008c621ab8b9d405a87
Mamy André-Ratsimbazafy <mamy_github@numforge.co>
2020-05-09 14:02:43 +0200

    whoopsie

--- weave/parallel_tasks.nim
+++ weave/parallel_tasks.nim
@@ -246,7 +246,7 @@ when isMainModule:
 
   block: # isReady
     proc sleepingLion(ms: int): int =
-      for _ 0 ..< ms:
+      for _ in 0 ..< ms:
         sleep(1)
       echo "--> Slept for ~", ms, " ms"
       return ms
